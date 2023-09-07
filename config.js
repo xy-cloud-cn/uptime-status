ffa75538f151677a8dbc7ca08e80ce0e9900d230
xy_cloud <85053859+xy-cloud-cn@users.noreply.github.com>
2023-09-07 21:42:43 +0800

    reinit

---
+++ config.js
@@ -0,0 +1,23 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'xy_cloud\'s Status',
+  //显示介绍
+  SiteDetail: 'xy_cloud的小窝的状态页面',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2291669-9f4d64567321d3c27d14711f',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+  ],
+};
